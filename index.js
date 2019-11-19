28f8aa4abe331216b7c518863abf53f40602393c
Victor Tran <victor.n.tran@outlook.com>
2019-11-18 16:48:23 -0800

    create toString() on Person, created Car and tasks, created Baby

--- index.js
+++ index.js
@@ -56,6 +56,11 @@ Person.prototype.poop = function() {
   this.stomach.length = 0;
 }
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
+}
+
+
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -70,8 +75,28 @@ Person.prototype.poop = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  const fuel = this.tank * this.milesPerGallon;
+  if (fuel < distance) {
+    this.tank = 0;
+    this.odometer += fuel;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+
+  const gallons = distance / this.milesPerGallon;
+  this.odometer += distance;
+  this.tank -= gallons
 }
 
 /*
@@ -81,8 +106,14 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
+}
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
